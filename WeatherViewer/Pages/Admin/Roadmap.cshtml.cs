341db457222ac340905e9868672321b3e9bc6ae6
John Muraski <62039431+jtmuraski@users.noreply.github.com>
2022-09-13 07:18:21 -0500

    Add About and Roadmap page
    Added the About Me and started the development Roadmap pages

---
+++ WeatherViewer/Pages/Admin/Roadmap.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherViewer.Pages.Admin
+{
+    public class RoadmapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
