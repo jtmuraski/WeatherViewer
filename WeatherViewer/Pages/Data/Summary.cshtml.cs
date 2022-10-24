796a0e5326bee8a5d17fef01e7421f19cc7d0e7c
John Muraski <62039431+jtmuraski@users.noreply.github.com>
2022-10-23 23:28:37 -0500

    Init
    Got page created, added function calls for retrieving data. Built initial tree grid

--- WeatherViewer/Pages/Data/Summary.cshtml.cs
+++ WeatherViewer/Pages/Data/Summary.cshtml.cs
@@ -4,6 +4,7 @@ using OpenWeatherSdk;
 using OpenWeatherSdk.Services;
 using OpenWeatherSdk.Models;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace WeatherViewer.Pages.Data
 {
@@ -17,11 +18,14 @@ namespace WeatherViewer.Pages.Data
             if(weatherSummary.ApiResponse == OpenWeatherSdk.Models.Enums.ApiCallStatus.Complete)
             {
                 WeatherData = weatherSummary.ShortSummaryCollection;
+                Debug.WriteLine("Number of records returned: " + weatherSummary.ShortSummaryCollection.Count);
                 Message = string.Empty;
             }
             else
             {
                 Message = "Oh no! Something went wrong and the summary weather data could not be retrieved.";
+                Message += weatherSummary.Message + "\n";
+                Message += weatherSummary.ApiResponse.ToString();
             }
         }
     }
